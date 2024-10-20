aebb2eff4c7e96579ecde19dd37f3adce28502b5
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-10-20 18:37:17 +1100

    basic tests

--- main.cpp
+++ main.cpp
@@ -2,16 +2,32 @@
 #include <iostream>
 #include "cartesianTree.hpp"
 
-int main() {
-    
-    std::vector<int> arr = {3, 2, 1, 6, 0, 5};
+void testTree(std::vector<int> arr){
     treeNode* root = constructRecTree(arr);
+    
+    std::cout << "Original tree:";
+    std::cout << std::endl;
+
+    for (int val : arr) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+    
+    std::cout << "Returning the cartesian tree back to a vec:";
+    std::cout << std::endl;
 
     std::vector<int> result = treeToVector(root);
     for (int val : result) {
         std::cout << val << " ";
     }
     std::cout << std::endl;
+}
+
+
+int main() {
+    std::vector<int> arr = {3, 2, 1, 6, 0, 5};
+    testTree(arr);
+
 
     return 0;
 }
