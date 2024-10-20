190fc5eee527f94a7ab3ee1a19b80df93df8781b
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-10-20 17:01:26 +1100

    cmake stuff dont know if i should be .gitignoring it

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/cartesianRecursive.cpp.o"
+  "CMakeFiles/main.dir/cartesianRecursive.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "output/main"
+  "output/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
