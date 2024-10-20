90897348bdd172bf4cfaa035e6320a571adde10d
MatthewAlexander2002 <matthewalexander009@gmail.com>
2024-10-20 18:30:58 +1100

    quick updates

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AdvAlgosCartesianTree
 Cpp Implementation of a Cartesian Tree
+
+Step 1:
+g++ -Wall -Wextra -g3 main.cpp cartesianRecursive.cpp -o output/main
+
+Step 2:
+./output/main
+
+Step 3:
+Profit!
--- main.cpp
+++ main.cpp
@@ -2,19 +2,6 @@
 #include <iostream>
 #include "cartesianTree.hpp"
 
-// example
-// TEST(Tree, Wikipedia) {
-//   std::vector<int> arr = {9,3,7,1,8,12,10,20,15,18,5};
-//   treeNode treeTop = constructRecTree(arr);
-//   EXPECT_EQ(sleepingDominoes(arr), arr); // fix naming
-// }
-
-
-// int main(int argc, char* argv[]) {
-//   ::testing::InitGoogleTest(&argc, argv);
-//   return RUN_ALL_TESTS();
-// }
-
 int main() {
     
     std::vector<int> arr = {3, 2, 1, 6, 0, 5};
